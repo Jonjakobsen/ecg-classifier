f45ddebdf11d3792a4b77eed521456e92c82bea9
Jonjakobsen <jonskaarupjakobsen@gmail.com>
2026-01-12 21:41:28 +0100

    refactor: load models once for reusable inference

--- src/ecg_classifier/inference.py
+++ src/ecg_classifier/inference.py
@@ -9,28 +9,28 @@ from importlib.resources import files
 LOGREG_PATH = files("ecg_classifier") / "artifacts" / "logreg.joblib"
 GRU_PATH = files("ecg_classifier") / "artifacts" / "gru.pt"
 
+# Load modeller én gang
+_LOGREG_MODEL = ECGLogReg()
+_LOGREG_MODEL.load(LOGREG_PATH)
+
+_GRU_MODEL = ECGGRU()
+_GRU_MODEL.load(GRU_PATH)
+
+
+label_map = {0: "NORMAL", 1: "NOT NORMAL"}
+
 
 def run_inference(path: str, fmt: str = "wfdb", model_type: str ="logreg") -> dict:
     signal = load_ecg(path, fmt)
 
 
     if model_type == "logreg":
+        pred = _LOGREG_MODEL.predict(signal)
 
-        model = ECGLogReg()
-        model.load(LOGREG_PATH)
-
-        pred = model.predict(signal)
-
-        label_map = {0: "NORMAL", 1: "NOT NORMAL"}
-    
     elif model_type == "gru":
+        pred = _GRU_MODEL.predict(signal)
+
         
-        model = ECGGRU()
-        model.load(GRU_PATH)
-
-        pred = model.predict(signal)
-
-        label_map = {0: "NORMAL", 1: "NOT NORMAL"}
 
     else:
         raise ValueError(
